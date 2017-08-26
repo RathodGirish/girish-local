92bfff127046c78d83867d5a9f53a1b171e9653a
Girish Rathod <rathodgirishk@gmail.com>
2017-08-26 10:26:19 +0530

    Update live database url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 	// the database url to connect
-	url: 'mongodb://192.168.1.140:27017/challengedb'
-	// url: 'mongodb://challengedb:foDKlUOnvl04KJV2cGn2NGGYCbKlWbxcKoMWba1cjzoROyZBOK4z6PCkaaLokRQrvDXstLSxxaW7eqtEjoJMoA==@challengedb.documents.azure.com:10255/?ssl=true'
+	// url: 'mongodb://192.168.1.140:27017/challengedb'
+	url: 'mongodb://challengedb:foDKlUOnvl04KJV2cGn2NGGYCbKlWbxcKoMWba1cjzoROyZBOK4z6PCkaaLokRQrvDXstLSxxaW7eqtEjoJMoA==@challengedb.documents.azure.com:10255/?ssl=true'
 }
