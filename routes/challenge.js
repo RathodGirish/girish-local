f843c334d0893ee3618d99f829cf79494874d513
Girish Rathod <rathodgirishk@gmail.com>
2017-09-01 18:19:32 +0530

    Set send grid key in environment varialbe

--- routes/challenge.js
+++ routes/challenge.js
@@ -35,7 +35,7 @@ function _getChallenges(req, res, next) {
         } else {
             json.status = '1';
             json.challenges = challenges;
-            json.variable = process.env.NODE_ENV;
+            json.variable = process.env.SENDG_KEY;
             res.send(json);
         }
     });
