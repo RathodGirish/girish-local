16fc5e76e7e5965d0e8c690202d12ecb01bf94be
Girish Rathod <rathodgirishk@gmail.com>
2017-09-27 13:52:05 +0530

    Updated new database url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 	// the database url to connect
-	// url: 'mongodb://192.168.1.140:27017/challengedb'
-	url: 'mongodb://challengedb:foDKlUOnvl04KJV2cGn2NGGYCbKlWbxcKoMWba1cjzoROyZBOK4z6PCkaaLokRQrvDXstLSxxaW7eqtEjoJMoA==@challengedb.documents.azure.com:10255/?ssl=true'
+	// url: 'mongodb://192.168.1.21:27017/challengedb'
+	// url: 'mongodb://challengedb:foDKlUOnvl04KJV2cGn2NGGYCbKlWbxcKoMWba1cjzoROyZBOK4z6PCkaaLokRQrvDXstLSxxaW7eqtEjoJMoA==@challengedb.documents.azure.com:10255/?ssl=true'
+	url: "mongodb://challengedb:ELNikbuLYKDxYuxivp2WDjdpmJsoACqdIgjOqsT2Qn8b9unG4SYKNrFMX3L1T7rvhtcYRc12iqiNiIBRKZ8wew==@challengedb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
 }
